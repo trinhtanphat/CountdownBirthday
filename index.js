e31dc316d4671485711085e6c39a6eb640bf7c41
trinhtanphat <89531707+trinhtanphat@users.noreply.github.com>
2022-12-15 11:24:47 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+var anhyeuem = new Date(2023, 3, 8, 0, 0, 0, 1000).getTime();
+function newYear() {
+     var ngayHienTai = new Date().getTime();
+     thoigianConLai = anhyeuem - ngayHienTai;
+     var giay = 1000;
+     var phut = giay * 60;
+     var gio = phut * 60;
+     var ngay = gio * 24;
+     var d = Math.floor(thoigianConLai / (ngay));
+     var h = Math.floor((thoigianConLai % (ngay)) / (gio));
+     var m = Math.floor((thoigianConLai % (gio)) / (phut));
+     var s = Math.floor((thoigianConLai % (phut)) / (giay));
+     document.getElementById("day").innerText = d;
+     document.getElementById("hour").innerText = h;
+     document.getElementById("minute").innerText = m;
+     document.getElementById("second").innerText = s;
+ }
+ setInterval(function () {
+     newYear()
+ }, 1000)
